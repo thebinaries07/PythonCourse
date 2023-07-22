f45b681ed486310395538cf6433215b2b63f1d07
thebinaries07 <140245780+thebinaries07@users.noreply.github.com>
2023-07-22 15:54:46 +0530

    Add files via upload

---
+++ tut2.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print(6)
+print(6.69)
+
+print(type("Hello World"))
+print(type(6))
+print(type(6.69))
---
+++ tut3.py
@@ -0,0 +1,17 @@
+# Comments
+# arnav
+# print("Arnav")
+
+# Variables
+# arnav = 69
+# name = "Arnav"
+# print(name, arnav)
+
+# Quiz Solution
+number = 5
+number_decimal = 3.33
+character = "Arnav"
+
+print(number, "is of type", type(number))
+print(number_decimal, "is of type", type(number_decimal))
+print(character, "is of type", type(character))
